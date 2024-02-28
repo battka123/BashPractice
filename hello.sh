a6fedba66dd95399f18358623079a3250b8c4833
Ivan <i.suschin@yandex.ru>
2024-02-28 14:30:35 -0500

    result

--- hello.sh
+++ hello.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
+# creates dirs from N to N+10
 
-cur=1
+# USAGE:
+# ./script.sh N
+# where N is positive number. Created dirs from N to N+10
 
-while [[ "$cur" -lt 11 ]];
+# to delete use
+#rmdir {1..10}
+
+usage()
+{
+    echo "USAGE: ./script.sh N, where N is positive number. Created dirs from N to N+10"
+}
+
+[[ $# -ne 1 ]] && usage && exit 1
+
+begin=$1
+
+[[ "$begin" -lt 0 ]] && echo Use positive number, not $begin && usage && exit 2
+
+end=$(( $begin + 10 ))
+
+while [[ "$begin" -le "$end" ]];
 do
-	echo "$cur"
-	cur=$((cur + 1))
+    if [[ -e "$begin" ]]
+    then
+        echo File $begin already exists
+        begin=$(( begin + 1 ))
+        continue
+    fi
+    mkdir -v "$begin"
+    begin=$(( begin + 1 ))
 done
